65ce76bea4c21506854b82992180977332eca415
shahan alam <shahanisright@gmail.com>
2021-07-17 17:43:43 +0800

    update: (1) added image (2) updated interface

--- web/static/scripts/index.js
+++ web/static/scripts/index.js
@@ -1,5 +1,5 @@
 let updateMessage = () => {
-    $('div:nth-of-type(2)').empty()
+    $('#weather-data').empty()
     let messaglist = [
         'Country',
         'Current Temp',
@@ -9,7 +9,7 @@ let updateMessage = () => {
         'Humidity', 'Clouds are'
     ]
     messaglist.forEach(msg => {
-        $('div:nth-of-type(2)').append(`<p>${msg}: <span></span></p>`);
+        $('#weather-data').append(`<p>${msg}: <span></span></p>`);
     })
 }
 let updateWeather = ()=>{
@@ -22,21 +22,22 @@ let updateWeather = ()=>{
         contentType:'application/json'
     }).done(function (data) { 
         $('.redmessage').text('');
-        if ($('div:nth-of-type(2) > p').length === 1){
+        if ($('#weather-data > p').length === 1){
             updateMessage()
         }
         data = data.data;        
         let temp = data.main;        
-        $('div:nth-of-type(2) > p:nth-of-type(1) > span').text(country.value);
-        $('div:nth-of-type(2) > p:nth-of-type(2) > span').text(temp.temp);
-        $('div:nth-of-type(2) > p:nth-of-type(3) > span').text(temp.temp_min);
-        $('div:nth-of-type(2) > p:nth-of-type(4) > span').text(temp.temp_max);
-        $('div:nth-of-type(2) > p:nth-of-type(5) > span').text(temp.feels_like);
-        $('div:nth-of-type(2) > p:nth-of-type(6) > span').text(temp.humidity);
-        $('div:nth-of-type(2) > p:nth-of-type(7) > span').text(data.weather[0].description);
+        $('#weather-data > p:nth-of-type(1) > span').text(country.value);
+        $('#weather-data > p:nth-of-type(2) > span').text(temp.temp);
+        $('#weather-data > p:nth-of-type(3) > span').text(temp.temp_min);
+        $('#weather-data > p:nth-of-type(4) > span').text(temp.temp_max);
+        $('#weather-data > p:nth-of-type(5) > span').text(temp.feels_like);
+        $('#weather-data > p:nth-of-type(6) > span').text(temp.humidity);
+        $('#weather-data > p:nth-of-type(7) > span').text(data.weather[0].description);
     }).fail(function (error) { 
         let msg = error.responseJSON;
         $('.redmessage').text(msg.message);
     })
 }
+
 $('button').on('click',updateWeather);
